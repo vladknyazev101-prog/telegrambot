19abeb3999ac75e0b6adacd431d437ae1cd02871
vladknyazev101-prog <vladknyazev101@gmail.com>
2025-08-07 16:52:05 +0300

    Update bot.py

--- telegram-bot/bot.py
+++ telegram-bot/bot.py
@@ -3,6 +3,7 @@ import json
 import time
 import random
 import asyncio
+from aiogram.types import Message
 from datetime import datetime
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
